a5a41209898034a63d57dc846d4d54f2c557e751
Shunsuke Michii <harukasan@me.com>
2016-08-10 16:45:22 +0900

    Fix javascript to follow idobata updates.

--- app/main_controller.rb
+++ app/main_controller.rb
@@ -121,32 +121,30 @@ class MainController < NSWindowController
   def webView(sender, didStartProvisionalLoadForFrame:frame)
     sender.windowScriptObject.evaluateWebScript <<-CODE
       (function(){
-        var onMessageCreated = function(user, store, router) {
-          return function(data){
-            var notify = false;
-            var mode = window.butter.notificationMode();
-            if (mode == "all") {
+        var onMessageCreated = function(user, store, router, data) {
+          var notify = false;
+          var mode = window.butter.notificationMode();
+          if (mode == "all") {
+            notify = true;
+          } else if (mode == "mention") {
+            if (data.message.mentions.indexOf(parseInt(user.get('id'))) >= 0) {
               notify = true;
-            } else if (mode == "mention") {
-              if (data.message.mentions.indexOf(parseInt(user.get('id'))) >= 0) {
-                notify = true;
-              }
             }
-            if (notify) {
-              store.find('message', data.message.id).then(function(message) {
-                var roomUrl = '/' + router.generate('room.index', message.get('room'));
-                var roomName = message.get('room.organization.name').toString() + ' / ' + message.get('room.name').toString();
-                var payload = {
-                  sender: data.message.senderName,
-                  roomName: roomName,
-                  text: data.message.bodyPlain,
-                  iconUrl: data.message.senderIconUrl,
-                  url: roomUrl
-                };
-                window.butter.notify(JSON.stringify(payload));
-              });
-            }
-          };
+          }
+          if (notify) {
+            store.find('message', data.message.id).then(function(message) {
+              var roomUrl = '/' + router.generate('room.index', message.get('room'));
+              var roomName = message.get('room.organization.name').toString() + ' / ' + message.get('room.name').toString();
+              var payload = {
+                sender: data.message.sender_name,
+                roomName: roomName,
+                text: data.message.body_plain,
+                iconUrl: data.message.sender_icon_url,
+                url: roomUrl
+              };
+              window.butter.notify(JSON.stringify(payload));
+            });
+          }
         };
 
         var onUnreadCountUpdated = function() {
@@ -155,10 +153,6 @@ class MainController < NSWindowController
 
         window.addEventListener('ready.idobata', function(e) {
           var container = e.detail.container;
-
-          var pusher = container.lookup('pusher:main');
-          var store  = container.lookup('store:main');
-          var router = container.lookup('router:main');
           var session = container.lookup('service:session');
 
           session.addObserver('user', function() {
@@ -169,9 +163,19 @@ class MainController < NSWindowController
 
             this.addObserver('totalUnreadMessagesCount', onUnreadCountUpdated);
             onUnreadCountUpdated.apply(user);
-            pusher.bind('message:created', onMessageCreated(user, store, router));
-          });
 
+            var router = container.lookup('router:main');
+            var stream = container.lookup("service:stream");
+            var store = container.lookup("service:store");
+
+            stream.on("event", function(e, data) {
+              switch (data.type) {
+                case "message_created":
+                  onMessageCreated(user, store, router, data.data);
+                  break;
+              }
+            });
+          });
         });
       })();
     CODE
